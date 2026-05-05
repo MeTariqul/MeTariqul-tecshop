b55f55ffc214343e4f8613d2cf59da30c4e83520
MeTariqul <users.noreply.github.com>
2026-05-06 03:19:16 +0600

    Fix python path for Vercel Serverless

--- techshop/techshop_proj/wsgi.py
+++ techshop/techshop_proj/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the techshop directory to the python path so Vercel can find techshop_proj.settings
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
